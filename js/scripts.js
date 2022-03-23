95a7536339468cd5d7ad702d72e2f5188d12f0fd
RicardoBasten <93449365+RicardoBasten@users.noreply.github.com>
2022-03-23 15:18:54 -0300

    Add files via upload
    Premier commit 230322

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// let menu = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.header .navbar');
+
+// menu.onclick = () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+    
+// };
+
+function toggle() {
+     var header =document.getElementById("header")
+     header.classList.toggle('active')
+}
+
+
